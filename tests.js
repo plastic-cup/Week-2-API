2b9bbf1bcf023afa230416e30b902c8d27a8b2d0
Danielwhyte <danwhy@gmail.com>
2015-05-19 16:55:29 +0100

    modified test

--- tests.js
+++ tests.js
@@ -1,15 +1,22 @@
 test('the http request "request" is finished and a success!',function(assert){
     var done = assert.async();
     var request = new XMLHttpRequest();
-    request.open('GET','https://api.instagram.com/v1/tags/nofilter/media/recent?client_id=b39dd08a617e403a88a19c7b870a240e');
-    request.send();
+    var getKey = new XMLHttpRequest();
+    getKey.open('GET','alohomora.txt')
+    getKey.send();
     setTimeout(function(){
-        var statusCode = request.status;
-        var ready = request.readyState;
-        console.log(ready);
-        equal(ready,4, 'request finished, yeeeeeeeeeeeah!');
-        console.log(statusCode);
-        equal(Math.floor(statusCode/100),2, 'status in the successes WHOOOOOOOOOOP');
-        done();
-    },2000);
+        var key = getKey.responseText;
+        request.open('GET','https://api.instagram.com/v1/tags/nofilter/media/recent?access_token=' + key);
+        console.log(getKey);
+        request.send();
+        setTimeout(function(){
+            var statusCode = request.status;
+            var ready = request.readyState;
+            console.log(ready);
+            equal(ready,4, 'request finished, yeeeeeeeeeeeah!');
+            console.log(statusCode);
+            equal(Math.floor(statusCode/100),2, 'status in the successes WHOOOOOOOOOOP');
+            done();
+        },2000);
+    }, 1000);
 })
