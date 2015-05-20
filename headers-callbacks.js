2e1ae2613679b83b88ede524c830b9ff325b4251
Danielwhyte <danwhy@gmail.com>
2015-05-20 14:05:09 +0100

    added functionality to select photos

--- headers-callbacks.js
+++ headers-callbacks.js
@@ -1,3 +1,5 @@
+var selected;
+
 var getKey = new XMLHttpRequest();
 getKey.open('GET','alohomora.txt');
 getKey.send();
@@ -16,11 +18,27 @@ function coolCallbackBack(info){
   var picElement;
   console.log('hey');
   for (var i = 0; i < info.data.length; i++){
-      console.log(info.data);
+    console.log(info.data[i]);
     picSources.push(info.data[i].images.thumbnail.url);
     picElement = document.createElement("img");
     picElement.setAttribute('src',picSources[i]);
     var picCell = document.getElementById('pic' + i);
     picCell.appendChild(picElement);
   }
+}
+
+var pictures = document.getElementsByClassName('pics');
+
+function select(){
+    var thumbUrl = this.firstChild.src;
+    var standardUrl = thumbUrl.split('/');
+    standardUrl.splice(5,1);
+    standardUrl = standardUrl.join('/');
+    selected = standardUrl;
+    console.log(selected);
+    return true;
+}
+
+for (var i = 0; i < pictures.length; i++){
+    pictures[i].addEventListener('click', select, false);
 }
