04e007e53b747dfafa907ca7139936652b976b18
rjmk <rafekennedy@gmail.com>
2015-05-20 11:24:21 +0100

    very basic img loading

--- headers-callbacks.js
+++ headers-callbacks.js
@@ -4,7 +4,7 @@ var getKey = new XMLHttpRequest();
 // getKey.send();
 // var key = getKey.responseText;
 // console.log(key);
-request.open('GET','https://api.instagram.com/v1/tags/nofilter/media/recent?access_token=TOKEN');
+request.open('GET','https://api.instagram.com/v1/tags/nofilter/media/recent?access_token=256725292.5c78bd6.917ec96983b74a2588e70ac87db92baa');
 request.setRequestHeader('accepts', 'text/javascript, application/javascript, application/ecmascript, application/x-ecmascript');
 request.setRequestHeader('script',/(?:java|ecma)script/);
 console.log(request);
@@ -12,7 +12,10 @@ request.send();
 
 
 function coolCallbackBack(info){
-  console.log(info);
+  var picSource = info.data[0].images.standard_resolution.url;
+  var picture = document.createElement("img")//.setAttribute('src',picSource);
+  document.body.appendChild(picture);
+  document.getElementsByTagName('img')[0].setAttribute('src',picSource);
 }
 
 // accepts script: "text/javascript, application/javascript, application/ecmascript, application/x-ecmascript"
