ef7e91e1042ffefc806d6a52d390bcb6aec14971
rjmk <rafekennedy@gmail.com>
2015-05-20 15:53:39 +0100

    maybe this is why?'

--- tests.js
+++ tests.js
@@ -1,10 +1,10 @@
 test('the photos on the page change',function(assert){
   var iframe = document.getElementById('iframe');
   var target = iframe.contentDocument || iframe.contentWindow.document;
-  var photosAtFirst = target.getElementById('photos');
+  var photosAtFirst = target.getElementById('photo-grid');
   var done = assert.async();
   setTimeout(function(){
-    var photosNow = target.getElementById('photos');
+    var photosNow = target.getElementById('photo-grid');
     notEqual(photosAtFirst,photosNow);
     done();
   },1000);
