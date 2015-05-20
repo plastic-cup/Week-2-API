f26b9c00bdbc04bcd4c8d2780b25616344eefb1e
rjmk <rafekennedy@gmail.com>
2015-05-20 14:44:06 +0100

    adds test that we never exceed 20 photos

--- tests.js
+++ tests.js
@@ -1,3 +1,14 @@
-test("old pics get deleted",function(){
-  
-})
+test("old pics get deleted",function(assert){
+  var done = assert.async();
+  var iframe = document.getElementById('iframe');
+  var target = iframe.contentDocument || iframe.contentWindow.document;
+  setTimeout(function(){
+    var photosAtFirst = target.getElementById('photos');
+    setTimeout(function(){
+      var photosNow = target.getElementById('photos');
+      notEqual(photosAtFirst,photosNow);
+      equal(photosNow.children.length, 20);
+      done();
+    },1000);
+  },100);
+});
