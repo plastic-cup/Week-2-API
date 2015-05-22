b2c4a125f01112451a13b32e0a16ceeb05dd1623
Anni Vaananen <anni.vaananen@gmail.com>
2015-05-22 16:06:41 +0100

    fixed test #5

--- javascript/tests.js
+++ javascript/tests.js
@@ -1,21 +1,26 @@
 test('the photos on the page change',function(assert){
   var iframe = document.getElementById('iframe');
   var target = iframe.contentDocument || iframe.contentWindow.document;
-  var photosAtFirst = target.getElementById('photo-grid');
+  var photosAtFirst = target.getElementById('photo-grid').children[0].children[0].children[0].children[0].children[0].children;
   var done = assert.async();
   setTimeout(function(){
-    var photosNow = target.getElementById('photo-grid');
+    var photosNow = target.getElementById('photo-grid').children[0].children[0].children[0].children[0].children[0].children;
     notEqual(photosAtFirst,photosNow);
     done();
-  },1000);
+  },6000);
 });
 
-test("Testthere an image on the page", function(){
+test("Testthere an image on the page", function(assert){
 var iframe = document.getElementById('iframe');
 var target = iframe.contentDocument || iframe.contentWindow.document;
-var image = target.getElementsByTagName("img")[0].src;
 
-notEqual(image, "", "Woop Well done dyyyd");
+var done = assert.async();
+  setTimeout(function(){
+  var image = target.getElementsByTagName("img")[0].src;
+  notEqual(image, "", "Woop Well done dyyyd");
+  done();
+  },200)
+
 
 });
 
@@ -51,18 +56,32 @@ updateTag(newTag);
 });
 
 
-test("Test does the var tag change when hit find", function(){
+test("Test does the var tag change when hit find", function(assert){
 
 var iframe =document.getElementById('iframe');
 var target = iframe.contentDocument || iframe.contentWindow.document;
 
-var tag1 = target.document.getElementById("input").value;
-target.getElementsByTagName('button').onclick();
-var tag2 = target.document.getElementById("input").value;
+  var done = assert.async();
 
-notEqual(tag2, tag1, "Successs");
+var tag1= target.tag;
+var tag2;
 
-});
+  setTimeout(function(){ 
+
+  target.getElementsByTagName('button')[0].click();
+;
+  },6000)
+
+    setTimeout(function(){ 
+    tag2 = target.getElementById("input").value;
+    console.log(tag2);
+    notEqual(tag2, tag1, "Successs");
+    done();
+    },8000)
+
+
+
+  });
 
 
 test("old pics get deleted",function(assert){
